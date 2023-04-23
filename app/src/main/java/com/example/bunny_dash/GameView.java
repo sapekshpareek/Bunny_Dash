d4fa71cb85ffcb97379473d4b8e068e4726c25ed
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-04-24 03:56:31 +0530

    Added Variables to GameView Class!

--- app/src/main/java/com/example/bunny_dash/GameView.java
+++ app/src/main/java/com/example/bunny_dash/GameView.java
@@ -1,4 +1,34 @@
 package com.example.bunny_dash;
 
+import android. content. Context;
+import android. graphics. Bitmap;
+import android.graphics.Paint;
+import android. graphics. Rect;
+import android. view. View;
+
+import java.util.ArrayList;
+
 public class GameView {
+    Bitmap background, ground, rabbit;
+    Rect rectBackground, rectGround;
+    Context context;
+    Handler handler;
+    final long UPDATE_MILLIS
+    Runnable runnable;
+    Paint textPaint = new Paint();
+    Paint healthPaint = new Paint();
+    float TEXT_SIZE = 120;
+    int points = 0;
+    int life = 3;
+    static int dWidth,dHeight;
+    Random random;
+    float rabbitX, rabbity;
+    float oldX;
+    float oldRabbitX;
+    ArrayList<Spike> spikes;
+    ArrayList<Explosion> explosions;
+
+    public GameView(Context context){
+        super(context);
+    }
 }
