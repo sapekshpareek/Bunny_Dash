8e160a5b990a4d3e2a036beaf9ddd53c324e2cdb
Sapeksh Pareek <sapekshpareek136@gmail.com>
2023-05-15 16:43:24 +0530

    Added file GameOver.java

---
+++ app/src/main/java/com/example/bunny_dash/GameOver.java
@@ -0,0 +1,15 @@
+package com.example.bunny_dash;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameOver extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, ) {
+        super.onCreate(savedInstanceState);
+    }
+}
