a68ad04f400565e3872034e9bf2b2ed83748fcd4
Sarthak Nagar <sarthaknagarjii@gmail.com>
2023-05-16 21:40:44 +0530

    Highest Scorer!

--- app/src/main/java/com/example/bunny_dash/GameOver.java
+++ app/src/main/java/com/example/bunny_dash/GameOver.java
@@ -1,15 +1,29 @@
 package com.example.bunny_dash;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.SharedMemory;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOver extends AppCompatActivity {
 
+    TextView tvPoints;
+    TextView tvHighest;
+    SharedPreferences sharedPreferences;
+    ImageView ivNewHighest;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, ) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_over);
+        tvPoints = findViewById(R.id.tvPoints);
+        tvHighest = findViewById(R.id.tvHighest);
+        
+
     }
 }
